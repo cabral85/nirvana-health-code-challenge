735b39466ae0a4b44438c8059d9b544fc693cd27
cabral85 <cabral85@gmail.com>
2021-11-30 12:40:14 -0300

    changed method to get response and calculate result

--- src/rules/coalesces.rules.ts
+++ src/rules/coalesces.rules.ts
@@ -8,15 +8,21 @@ const listUrls = process.env.URLS;
 // eslint-disable-next-line max-len
 const urlRegex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
 
+interface apiResponseInterface {
+  deductible: number,
+  stop_loss: number,
+  oop_max: number,
+};
+
 const getCoalesce = async (memberId: number) => {
   const apiResponse = await getApiResponse(memberId);
 
   if (strategy === 'average') {
-    return getAverage();
+    return getAverage(apiResponse);
   } else if (strategy === 'maximum') {
-    return getMaximum();
+    return getMaximum(apiResponse);
   } else if (strategy === 'minimum') {
-    return getMinimum();
+    return getMinimum(apiResponse);
   }
 };
 
@@ -24,7 +30,7 @@ const getApiResponse = async (memberId: number) => {
   const regex = new RegExp(urlRegex);
   const urls = listUrls?.split(',');
   // eslint-disable-next-line no-array-constructor
-  const response = new Array<any>();
+  const response = new Array<apiResponseInterface>();
   for (const url in urls) {
     if (url.match(regex)) {
       const result = await axios.get(`${url}/${memberId}`);
@@ -34,16 +40,16 @@ const getApiResponse = async (memberId: number) => {
   return response;
 };
 
-const getAverage = async () => {
-  return;
+const getAverage = async (apiResponse: Array<apiResponseInterface>) => {
+  return apiResponse;
 };
 
-const getMaximum = async () => {
-  return;
+const getMaximum = async (apiResponse: Array<apiResponseInterface>) => {
+  return apiResponse;
 };
 
-const getMinimum = async () => {
-  return;
+const getMinimum = async (apiResponse: Array<apiResponseInterface>) => {
+  return apiResponse;
 };
 
 export { getCoalesce };
